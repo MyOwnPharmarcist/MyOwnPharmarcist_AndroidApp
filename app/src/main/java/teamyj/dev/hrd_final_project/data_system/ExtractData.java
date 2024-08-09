9e9da6aa94bce6df2d2a1b3d18b6d33d9b68587f
seoch <seocheu59@naver.com>
2024-08-09 14:14:20 +0900

    심성민 24/08/09 14:12

--- app/src/main/java/teamyj/dev/hrd_final_project/data_system/ExtractData.java
+++
@@ -1,9 +0,0 @@
-package teamyj.dev.hrd_final_project.data_system;
-
-import android.content.res.AssetManager;
-
-public class ExtractData {
-    public void extract(AssetManager assetManager) {
-
-    }
-}
