d1e98ede60f4cc5326c50a46b5c6084b1e06e317
KorYejinLee <Koryjlee@gmail.com>
2024-08-21 14:52:41 +0900

    20240821 14:52 이예진 drug_product_final.csv, drug_products.db
    Former-commit-id: 96aedd77ad47d8d151b8a24bc8b9f9e9918c5596

--- app/src/main/java/teamyj/dev/hrd_final_project/data_system/DrugProductsCreate.java
+++ app/src/main/java/teamyj/dev/hrd_final_project/data_system/DrugProductsCreate.java
@@ -1,6 +1,19 @@
 package teamyj.dev.hrd_final_project.data_system;
 
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.CHART;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.EE_DOC_DATA;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.ENTP_NAME;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.ETC_OTC_CODE;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.INDUTY_TYPE;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.ITEM_NAME;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.STORAGE_METHOD;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.TABLE_DRUG_PRODUCTS;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.UD_DOC_DATA;
+import static teamyj.dev.hrd_final_project.data_system.DrugProductsDBOpenHelper.VALID_TERM;
+
+import android.content.ContentValues;
 import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -23,23 +36,30 @@ public class DrugProductsCreate {
             String[] buffer;
             String line;
             int count;
+            // 에셋에서 csv 파일을 읽어서 String으로 반환
             while ((line = bufferedReader.readLine()) != null) {
                 stringBuilder.append(line);
+                // """ 단위로 split을 했을 때 배열의 크기 = 18개
                 buffer = line.split("\"\"\"");
-                if((count = buffer.length) == 19) {
-                    processingDrugProductsData(stringBuilder.toString());
-                } else {
-                    while (count != 19) {
-                        line = bufferedReader.readLine();
+                count = buffer.length;
+                if(line.endsWith("\"\"\"")) {
+                    count++;
+                }
+                while (count < 19) {
+                    line = bufferedReader.readLine();
+                    if (count != 18) {
                         stringBuilder.append(line);
                         buffer = line.split("\"\"\"");
                         count += buffer.length - 1;
-                        if(line.endsWith("\"\"\"")) {
-                            count++;
-                        }
+                    } else {
+                        stringBuilder.append("\n").append(line);
                     }
-                    processingDrugProductsData(stringBuilder.toString());
+
+                    if (line.endsWith("\"\"\"")) {
+                        count++;
+                    }
                 }
+                processingDrugProductsData(stringBuilder.toString());
 
                 stringBuilder.setLength(0);
             }
@@ -57,12 +77,25 @@ public class DrugProductsCreate {
     }
 
     private void processingDrugProductsData(String product_info) {
-        // info를 분리하여 저장할 객체
-        String[] data = null;
-
         // 전처리
         String[] preproessing = product_info.split("\"\"\"");
-        System.out.printf("");
 
+        addDrugProductsData(preproessing);
+    }
+
+    private void addDrugProductsData(String[] data)  {
+        SQLiteDatabase db = drugProductsDBOpenHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(ITEM_NAME, data[1]);
+        values.put(ENTP_NAME, data[3]);
+        values.put(ETC_OTC_CODE, data[5]);
+        values.put(CHART, data[7]);
+        values.put(STORAGE_METHOD, data[9]);
+        values.put(VALID_TERM, data[11]);
+        values.put(INDUTY_TYPE, data[13]);
+        values.put(EE_DOC_DATA, data[15]);
+        values.put(UD_DOC_DATA, data[17]);
+        db.insertWithOnConflict(TABLE_DRUG_PRODUCTS, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+        Log.i("drug", data[1]);
     }
 }
