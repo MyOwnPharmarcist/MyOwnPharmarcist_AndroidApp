d229123d74fd8873163ff2d710f8df2c62e0f7ad
seoch <seocheu59@naver.com>
2024-08-28 15:25:30 +0900

    심성민 24.08.28 15:25 터치 리스너 방식 변경

--- app/src/main/java/teamyj/dev/hrd_final_project/layout/SearchDrugsFragment.java
+++ app/src/main/java/teamyj/dev/hrd_final_project/layout/SearchDrugsFragment.java
@@ -7,11 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -24,30 +25,17 @@ import teamyj.dev.hrd_final_project.main_system.CustomApplication;
 public class SearchDrugsFragment extends Fragment {
 
     View view;
-    private ImageView selectedImageView;  // 현재 선택된 ImageView
-
-    // shape의 id 배열
-    int[] shapeIds = {
-            R.id.shape0, R.id.shape1, R.id.shape2, R.id.shape3, R.id.shape4,
-            R.id.shape5, R.id.shape6, R.id.shape7, R.id.shape8, R.id.shape9,
-            R.id.shape10
-    };
-
-    // color의 id 배열
-    int[] colorIds = {
-            R.id.colorall, R.id.colorwhite, R.id.coloryellow, R.id.colorornage, R.id.colorpink,
-            R.id.colorred, R.id.colorbrown, R.id.colorlightgreen, R.id.colorgreen, R.id.colorturquoise,
-            R.id.colorblue, R.id.colorsodomy, R.id.coloramethyst, R.id.colorpurple, R.id.colorgray,
-            R.id.colorblack, R.id.colornull
-    };
-
-    // formulation의 id 배열
-    int[] formulationIds = {
-            R.id.formulation5, R.id.formulation1, R.id.formulation2, R.id.formulation3, R.id.formulation4
-    };
+    private ImageView selectedShape;  // 현재 선택된 ImageView
+    private ImageView selectedColor;
+    private ImageView selectedFormulation;
 
     ListDataGettable listHelper;
     private ListView listView;
+
+    String name = "";
+    String shape = "";
+    String color = "";
+    String codeName = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,10 +44,18 @@ public class SearchDrugsFragment extends Fragment {
 
         DBHelperGettable application = CustomApplication.getInstance();
         listHelper = application.getList();
+
+        selectedShape = view.findViewById(R.id.shape0_image);
+        selectedShape.setBackgroundColor(Color.BLUE);
+//        selectedColor = view.findViewById(R.id.colorall_image);
+//        selectedColor.setBackgroundColor(Color.BLUE);
+//        selectedFormulation = view.findViewById(R.id.formulation5_image);
+//        selectedFormulation.setBackgroundColor(Color.BLUE);
+
         // 각 스크롤 뷰 내의 이미지들에 클릭 리스너 설정
-        setupClickListeners((LinearLayout) ((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.shapesearch)).getChildAt(0)));
-        setupClickListeners((LinearLayout) ((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.colorsearch)).getChildAt(0)));
-        setupClickListeners((LinearLayout) ((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.formulationsearch)).getChildAt(0)));
+        setupClickListeners((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.shapesearch)).getChildAt(0));
+        setupClickListeners((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.colorsearch)).getChildAt(0));
+        setupClickListeners((LinearLayout) ((HorizontalScrollView) view.findViewById(R.id.formulationsearch)).getChildAt(0));
 
 
         Button searchButton = view.findViewById(R.id.searchbtn);
@@ -78,7 +74,7 @@ public class SearchDrugsFragment extends Fragment {
     private void setupClickListeners(LinearLayout layout) {
         for (int i = 0; i < layout.getChildCount(); i++) {
             View child = layout.getChildAt(i);
-            if (child instanceof ImageView) {
+            if (child instanceof FrameLayout) {
                 child.setOnClickListener(this::onImageClick);
             }
         }
@@ -86,35 +82,24 @@ public class SearchDrugsFragment extends Fragment {
 
     // ImageView 클릭 시 호출되는 메서드
     public void onImageClick(View view) {
-        int id = view.getId();
+        ImageView image = (ImageView) ((FrameLayout)view).getChildAt(0);
+        image.setBackgroundColor(Color.BLUE);
 
-        // 기존에 선택된 ImageView의 테두리를 제거
-        if (selectedImageView != null) {
-            selectedImageView.setBackgroundColor(Color.TRANSPARENT);
+        TextView text = (TextView) ((FrameLayout)view).getChildAt(1);
+
+        if(view.getParent().equals(selectedShape.getParent().getParent())) {
+            selectedShape.setBackgroundColor(Color.TRANSPARENT);
+            selectedShape = image;
+            shape = text.getText().toString();
+        } else if(view.getParent().equals(selectedColor.getParent().getParent())) {
+            selectedColor.setBackgroundColor(Color.TRANSPARENT);
+            selectedColor = image;
+            color = text.getText().toString();
+        } else {
+            selectedFormulation.setBackgroundColor(Color.TRANSPARENT);
+            selectedFormulation = image;
+            codeName = text.getText().toString();
         }
-
-        // 선택된 ImageView에 테두리 추가
-        view.setBackgroundColor(Color.BLUE);
-        selectedImageView = (ImageView) view;  // 현재 선택된 ImageView를 저장
-
-        // 각 그룹의 ID 배열에 해당하는지 확인
-        if (isInArray(shapeIds, id)) {
-            Toast.makeText(getContext(), "Shape 선택됨: " + id, Toast.LENGTH_SHORT).show();
-        } else if (isInArray(colorIds, id)) {
-            Toast.makeText(getContext(), "Color 선택됨: " + id, Toast.LENGTH_SHORT).show();
-        } else if (isInArray(formulationIds, id)) {
-            Toast.makeText(getContext(), "Formulation 선택됨: " + id, Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    // ID가 주어진 배열에 포함되어 있는지 확인하는 메서드
-    private boolean isInArray(int[] ids, int id) {
-        for (int i : ids) {
-            if (i == id) {
-                return true;
-            }
-        }
-        return false;
     }
 
 //    // DB에서 데이터를 가져오는 함수 (예시)
@@ -140,12 +125,10 @@ public class SearchDrugsFragment extends Fragment {
     }
 
     private String makeSQLCondition() {
-        String shape;
-        String color;
-        String codeName;
+
 
         String result = "";
-                                                                                     
+
         return result;
     }
 
